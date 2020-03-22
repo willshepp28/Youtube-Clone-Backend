1e74ad9b1799791f6a331b13e3cdd22ad2478817
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-22 16:54:30 -0400

    updated .env; install dotenv; updated config.js;

--- db/config/config.js
+++ db/config/config.js
@@ -1,26 +1,24 @@
-JSON.parse(JSON.stringify({
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    logging: console.log
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
+    ssl: true
   }
-}));
+}
+
+
