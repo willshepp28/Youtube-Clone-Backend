e20b2554766767870ece7a57a8d0a5fe9061e112
Will Sheppard <28759252+willshepp28@users.noreply.github.com>
2020-03-22 16:59:45 -0400

    changed the way we initalize sequelize constructor for production in index.js

--- db/models/index.js
+++ db/models/index.js
@@ -9,8 +9,15 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    },
+  })
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
